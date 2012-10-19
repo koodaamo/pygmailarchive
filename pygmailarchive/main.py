9899b220b9d30f9f39e31a1a962e36ad81f713bb
Petri Savolainen <petri.savolainen@iki.fi>
2012-10-19 17:09:37 +0300

    refactoring & switching to the simplified arg & config parsing

--- pygmailarchive/main.py
+++ pygmailarchive/main.py
@@ -4,71 +4,60 @@ import sys
 import math
 import getpass
 import mailbox
+import logging
 
+from argparsing import get_parser
+from util import gmail, makeFSCompatible, mailfolder
+from config import logger
 
-from ConfigParser import SafeConfigParser
-from pygmailarchive.argparsing import get_parser
-from pygmailarchive.util import gmail, logger, makeFSCompatible, mailfolder
-from pygmailarchive.config import getconfig, FOLDERLIST_CMD
+# if you use this from third-party software, pass a list of parents to apply and a logger
 
-# if you use this from third-party software, pass both a parser instance and parsed args
-# and optionally a logger
+def run_archiver(parents=[], logger=logger):
 
-def run(parser=None, args=None, thirdpartylogger=None):
+    # if we're given parents, third-party sw wants to mix with the barebones parser
+    bbones = True if parents else False
+    argparser = get_parser(parents=parents, barebones=bbones)
+    args = argparser.parse_args()
 
-    # COMMAND-LINE ARGS
-    argparser = parser or get_parser()
-    args = args or argparser.parse_args()
-
-    # CONFIGURATION (including config file read)
-    cfg = SafeConfigParser()
-    cmd, user, passwd, includes, excludes, archivedir, loglevel = getconfig(args, cfg)
-
-    includes = includes or []
-    excludes = excludes or []
-
-    # log levels are tied to args and their parsing
-    if loglevel and not thirdpartylogger:
-        logger.setLevel(loglevel)
+    loglevel= getattr(logging, args.loglevel.upper())
+    logger.setLevel(loglevel)
 
     # Require either archival or listing operation before proceeding.
-    if not cmd:
+    if not (args.list_folders or args.archivedir):
         argparser.print_help()
         sys.exit()
 
     # Auth & connect
-    username = user or raw_input("Username: ")
-    password = passwd or getpass.getpass()
-
+    username = args.username or raw_input("Username: ")
+    password = args.password or getpass.getpass()
     with gmail(username, password) as imapcon:
-
        logger.info("Connected")
-       # Get folder listing first
-       all_folders = [fdata[2] for fdata in imapcon.list_folders()]
+
+       # Get folder listing and folder path separator from server; they will be needed.
+       # Also make a slash ('/') -delimited list of all folders for convenience.
+       # Note: an IMAP folder entry contains the full path, not just the leaf.
+       folders = [fdata[2] for fdata in imapcon.list_folders()]
+       fsep = imapcon.namespace().personal[0][1]
+       folders_slashdelimited = [fname.replace(fsep, u'/') for fname in folders]
 
        # If requested (-f/--folders), just output it & exit
-       if cmd == FOLDERLIST_CMD:
-          sys.exit("Folders on server: %s" % " ".join(all_folders))
+       if args.list_folders:
+          sys.exit("Folders on server: %s" % ", ".join(folders_slashdelimited))
 
-       # Parse, check and apply folder constraints given by user.
-       # Constraints are given using '/' as the folder path separator
-       fldrsep = imapcon.namespace().personal[0][1]
-       fixsep = lambda pths: [unicode(pth.replace('/', fldrsep)) for pth in pths]
-
-       if includes or excludes:
-           inv_inc = [f for f in fixsep(includes) if f not in all_folders]
-           inv_exc = [f for f in fixsep(excludes) if f not in all_folders]
-           invalids = "' and '".join(inv_inc + inv_exc)
-           if invalids:
-              sys.exit("Invalid include/exclude folder names: '%s'" % invalids)
-
-       folders = includes or [f for f in all_folders if f not in excludes]
-       logger.info("Archiving '%s' to maildir at %s" % ("', '".join(folders), archivedir))
-
-       # Ok. Set up the archive dir.
-       archive = mailbox.Maildir(archivedir)
+       # Apply include/exclude options on the list of folders found on server, after
+       # making sure they exist on server.
+       invalids = []
+       if args.include:
+           invalids += [fld for fld in args.include if fld not in folders_slashdelimited]
+       if args.exclude:
+           invalids += [fld for fld in args.exclude if fld not in folders_slashdelimited]
+       if invalids:
+           sys.exit("Invalid include/exclude folder names: '%s'" % invalids)
+       folders = args.include or (set(folders_slashdelimited) - set(args.exclude))
 
        # Archive messages!
+       logger.info("Archiving '%s' to %s" % ("', '".join(folders), args.archivedir))
+       archive = mailbox.Maildir(args.archivedir)
        for foldername in folders:
            select_info = imapcon.select_folder(foldername)
            if select_info['EXISTS'] == 0:
@@ -82,7 +71,7 @@ def run(parser=None, args=None, thirdpartylogger=None):
            uidvalidity = select_info['UIDVALIDITY']
            logger.debug("... UID validity: %s" % uidvalidity)
 
-           parts = [makeFSCompatible(unicode(prt)) for prt in foldername.split(fldrsep)]
+           parts = [makeFSCompatible(unicode(prt)) for prt in foldername.split(fsep)]
            fsname = '.'.join(parts)
 
            with mailfolder(archive, fsname) as (folder, cached_uid_info):
